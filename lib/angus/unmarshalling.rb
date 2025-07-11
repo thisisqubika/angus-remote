f5ea9fd2c4e2e2c515523604457a6af8a4563a11
Gustavo Vera Scuderi <gustavo.vera@bancard.com.py>
2025-07-11 11:31:06 -0300

    Updated BigDecimal unmarshalling

--- lib/angus/unmarshalling.rb
+++ lib/angus/unmarshalling.rb
@@ -21,10 +21,10 @@ module Angus
       when :date_time
         DateTime.iso8601(scalar)
       when :decimal
-        if BigDecimal.respond_to?(:new)
-          BigDecimal.new(scalar) # Para Ruby < 2.6
-        else
-          BigDecimal(scalar) # Para Ruby 2.6+
+        begin
+          BigDecimal(scalar) # Para Ruby 2.4+, JRuby
+        rescue NoMethodError
+          BigDecimal.new(scalar) # Para Ruby < 2.4
         end
       when :object
         scalar
